524075478bedfb6ec9610aa981e6b32f11ab7755
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:31:23 -0500

    Se corrige el valor esperado del test Kata 7

--- src/test/java/katas/Kata7Test.java
+++ src/test/java/katas/Kata7Test.java
@@ -1,15 +1,23 @@
 package katas;
 
+import model.BoxArt;
+import model.MovieList;
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata7Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata7.execute(), equalTo(4));
+        Assert.assertThat(Kata7.execute().size(), equalTo(4));
+
     }
 }
