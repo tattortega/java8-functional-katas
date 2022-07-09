5526cf2d62fa467b2573e6f2d276990120746931
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:31:49 -0500

    Se resuelve Kata 9

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -1,15 +1,12 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import model.Bookmark;
-import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +17,22 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        return movieLists
+                .stream()
+                .flatMap(movies -> movies.getVideos().stream().map(videos ->
+                        ImmutableMap.of(
+                                "id", videos.getId(),
+                                "title", videos.getTitle(),
+                                " middle interesting moment time",
+                                videos.getInterestingMoments()
+                                        .stream()
+                                        .filter(moment -> moment.getType().equals("Middle")).findAny().get().getTime(),
+                                "box art",
+                                videos.getBoxarts()
+                                         .stream()
+                                        .reduce((acumulated, element) -> acumulated.getWidth() >= element.getWidth() &&
+                                                acumulated.getHeight() >= element.getHeight() ? element : acumulated)
+                                        .get().getUrl())
+                )).collect(Collectors.toList());
     }
 }
