5bc74032449cffd3e7185f8f2490c9e949bd02b8
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:33:06 -0500

    Se corrige el valor esperado del test Kata 11

--- src/test/java/katas/Kata11Test.java
+++ src/test/java/katas/Kata11Test.java
@@ -2,14 +2,20 @@ package katas;
 
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata11Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata11.execute(), equalTo(4));
+        Assert.assertThat(Kata11.execute().size(), equalTo(2));
     }
 }
