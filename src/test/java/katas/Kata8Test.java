5b9eea54afe887968fdcb791e8d3612e373e29a4
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:31:42 -0500

    Se corrige el valor esperado del test Kata 8

--- src/test/java/katas/Kata8Test.java
+++ src/test/java/katas/Kata8Test.java
@@ -1,15 +1,24 @@
 package katas;
 
+import com.codepoetics.protonpack.StreamUtils;
+import model.Bookmark;
+import model.Movie;
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata8Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata8.execute(), equalTo(4));
+        Assert.assertThat(Kata8.execute().size(), equalTo(3));
+
     }
 }
