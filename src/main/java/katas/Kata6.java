f9c626b904774a44da0f1913abab920107341f69
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:30:39 -0500

    Se resuelve Kata 6

--- src/main/java/katas/Kata6.java
+++ src/main/java/katas/Kata6.java
@@ -1,8 +1,10 @@
 package katas;
 
+import model.BoxArt;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.Collection;
 import java.util.List;
 
 /*
@@ -14,6 +16,11 @@ public class Kata6 {
     public static String execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return "someUrl";
+        return movies.stream()
+                .map(Movie::getBoxarts)
+                .flatMap(Collection::stream)
+                .reduce((box1, box2) -> box1.getWidth() > box2.getWidth() ? box1 : box2)
+                .map(BoxArt::getUrl)
+                .get();
     }
 }
