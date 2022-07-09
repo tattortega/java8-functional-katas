eba33ff1ea2c88c2073ea01d038d715c2c129fb1
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:30:02 -0500

    Se resuelve Kata 4

--- src/main/java/katas/Kata4.java
+++ src/main/java/katas/Kata4.java
@@ -1,13 +1,12 @@
 package katas;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import model.BoxArt;
 import model.MovieList;
 import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve id, title, and a 150x200 box art url for every video
@@ -18,6 +17,13 @@ public class Kata4 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", new BoxArt(150, 200, "url")));
+        return movieLists.stream().flatMap(movies ->
+                movies.getVideos().stream().map(movie ->
+                        ImmutableMap.of(
+                                "id", movie.getId(),
+                                "title", movie.getTitle(),
+                                "boxart", movie.getBoxarts().stream()
+                                        .filter(boxArt -> boxArt.getWidth().equals(150) && boxArt.getHeight().equals(200))))
+        ).collect(Collectors.toList());
     }
 }
