10ae226d34844a503d5ca8167a4f35d32d01247c
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:32:48 -0500

    Se corrige el valor esperado del test Kata 10

--- src/test/java/katas/Kata10Test.java
+++ src/test/java/katas/Kata10Test.java
@@ -2,14 +2,20 @@ package katas;
 
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata10Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata10.execute(), equalTo(4));
+        Assert.assertThat(Kata10.execute().size(), equalTo(2));
+
     }
 }
