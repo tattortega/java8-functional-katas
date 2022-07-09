bd88866887086e336f63526471a50767e3824124
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:31:57 -0500

    Se corrige el valor esperado del test Kata 9

--- src/test/java/katas/Kata9Test.java
+++ src/test/java/katas/Kata9Test.java
@@ -1,15 +1,23 @@
 package katas;
 
+import model.BoxArt;
+import model.InterestingMoment;
+import model.MovieList;
 import org.junit.Assert;
 import org.junit.Test;
+import util.DataUtil;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 
 public class Kata9Test {
 
     @Test
     public void testExecute() {
-        Assert.assertThat(Kata9.execute(), equalTo(4));
+        Assert.assertThat(Kata9.execute().size(), equalTo(4));
     }
 }
