2451aa1ca07d67a76acb0610a74a05b8773551ce
Ricardo Ortega <tattortega.28@gmail.com>
2022-07-08 20:31:33 -0500

    Se resuelve Kata 8

--- src/main/java/katas/Kata8.java
+++ src/main/java/katas/Kata8.java
@@ -10,6 +10,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Combine videos and bookmarks by index (StreamUtils.zip) (https://github.com/poetix/protonpack)
@@ -21,8 +22,8 @@ public class Kata8 {
         List<Movie> movies = DataUtil.getMovies();
         List<Bookmark> bookMarks = DataUtil.getBookMarks();
 
-        // StreamUtils.zip()
-
-        return ImmutableList.of(ImmutableMap.of("videoId", 5, "bookmarkId", 3));
+        return StreamUtils.zip(movies.stream(), bookMarks.stream(), (movie, bookMark) ->
+                        ImmutableMap.of("videoId", movie.getId(), "bookmarkId", bookMark.getId()))
+                .collect(Collectors.toList());
     }
 }
